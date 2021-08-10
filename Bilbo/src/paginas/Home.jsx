aa5d292606998d963c76fbe4058eb353ef5aec24
Edilene Shizue <edilene.shizue@aluno.ifsp.edu.br>
2021-08-09 23:41:37 -0300

    tentativa frustrante de react

---
+++ Bilbo/src/paginas/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="titulo-pagina">Pets</h2>
+      </div>
+      <section className="container flex flex--centro">
+      <article className="cartao post">
+        <h2 className="cartao__titulo">o Bilbo</h2>
+        <p className="cartao__texto">O Bilbo é um cachorrrinho muito fofo e bobo, que ama raçãozinha, garrafinha, ossinhos. :3</p>
+      </article>
+    </section>
+
+    </main>
+  )
+}
+
+export default Home
---
+++ Bilbo/src/paginas/Sobre.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+import '../assets/css/componentes/cartao.css'
+
+const Sobre = () => {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="titulo-pagina">Sobre o Bilbo</h2>
+      </div>
+      <section className="container flex flex--centro">
+        <article className="cartao">
+          O Bilbo é um Yorkshire, mesmo não parecendo :3 mas é muito fofinho e bonzinho.
+        </article>
+        
+      </section>
+    </main>
+  )
+}
+
+export default Sobre
